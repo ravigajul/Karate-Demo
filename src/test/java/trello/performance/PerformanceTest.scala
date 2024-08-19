e6832359caf79a7abb321b6e7a88b51c2726cdcf
Ravi Kumar Gajul <51070896+ravigajul@users.noreply.github.com>
2024-08-18 17:43:54 -0700

    added injection examples to scala file (#3)
    * added scala example
    
    * added scenarios

--- src/test/java/trello/performance/PerformanceTest.scala
+++ src/test/java/trello/performance/PerformanceTest.scala
@@ -16,4 +16,17 @@ class PerformanceTest extends Simulation {
     create.inject(rampUsers(2) during (5 seconds)).protocols(protocol),
   )
 
+// setUp(
+//   create.inject(
+//     nothingFor(4), // 1
+//     atOnceUsers(10), // 2
+//     rampUsers(10).during(5), // 3
+//     constantUsersPerSec(20).during(15), // 4
+//     constantUsersPerSec(20).during(15).randomized, // 5
+//     rampUsersPerSec(10).to(20).during(10.minutes), // 6
+//     rampUsersPerSec(10).to(20).during(10.minutes).randomized, // 7
+//     stressPeakUsers(1000).during(20) // 8
+//   ).protocols(httpProtocol)
+// )
+
 }
